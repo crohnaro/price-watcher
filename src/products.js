135434094bbd8d42d9e739cc0fbcbf3bb6180a5f
crohnaro <vinicioscararine95@gmail.com>
2025-06-07 08:55:10 -0300

    adding xpg memory

--- src/products.js
+++ src/products.js
@@ -1,4 +1,11 @@
-export const TARGETS = [{
-    url: 'https://www.kabum.com.br/produto/382771/memoria-ram-corsair-vengeance-rgb-32gb-2x16gb-6200mhz-ddr5-cl36-preto-cmh32gx5m2b6200c36',
-    selector: '#blocoValores > div.sc-a24aba34-3.hSVqxN > div.sc-a24aba34-1.cpLDBn > div > h4'
-}]
+export const TARGETS =
+    [
+        {
+            url: 'https://www.kabum.com.br/produto/382771/memoria-ram-corsair-vengeance-rgb-32gb-2x16gb-6200mhz-ddr5-cl36-preto-cmh32gx5m2b6200c36',
+            selector: '#blocoValores > div.sc-a24aba34-3.hSVqxN > div.sc-a24aba34-1.cpLDBn > div > h4'
+        },
+        {
+            url: 'https://www.kabum.com.br/produto/526516/memoria-ram-xpg-lancer-rgb-32gb-2x16gb-6000mhz-ddr5-cl30-preto-ax5u6000c3016g-dclarbk?utm_id=22429436051&gad_source=1&gad_campaignid=22429436051&gbraid=0AAAAADx-HyE964u_Iel76bGfQycZj2fwG&gclid=Cj0KCQjwxo_CBhDbARIsADWpDH52oRH3xefyPM-p_NMlDMGGRYV0QhkuaJSUEcR4dqWAkNUonIcIcbEaAu6UEALw_wcB',
+            selector: '#blocoValores > div.sc-a24aba34-3.hSVqxN > div.sc-a24aba34-1.cpLDBn > div > h4'
+        }
+    ]
